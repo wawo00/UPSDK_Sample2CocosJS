0f98bfba261eb8708d4b02fabbc544f0e2abab7f
wanlei <roy.wan@upltv.com>
2018-10-30 14:23:19 +0800

    增加头条，关闭统计包

--- app/src/org/cocos2dx/javascript/AppActivity.java
+++ app/src/org/cocos2dx/javascript/AppActivity.java
@@ -27,8 +27,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 
-import com.hola.pay.HolaPay;
-import com.hola.sdk.HolaAnalysis;
+
 import com.up.ads.UPAdsSdk;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
@@ -40,14 +39,14 @@ public class AppActivity extends Cocos2dxActivity {
     protected void onCreate(Bundle savedInstanceState) {
         UPAdsSdk.init(this, UPAdsSdk.UPAdsGlobalZone.UPAdsGlobalZoneAuto);
 
-        new Handler(getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Log.e("analysis", count+"");
-                count++;
-                setHolyAnalysis();
-            }
-        }, 4 * 1000);
+//        new Handler(getMainLooper()).postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Log.e("analysis", count+"");
+//                count++;
+//                setHolyAnalysis();
+//            }
+//        }, 4 * 1000);
 
 
         super.setEnableVirtualButton(false);
@@ -64,13 +63,13 @@ public class AppActivity extends Cocos2dxActivity {
         // DO OTHER INITIALIZATION BELOW
 
     }
-    public void setHolyAnalysis(){
-        HolaAnalysis.init(getApplicationContext(), "888888", "32400");
-        String openID = HolaAnalysis.getOpenId(AppActivity.this);
-        Log.e("analysis_openid", openID);
-        HolaAnalysis.setCustomerId("666666");
-        HolaAnalysis.log("hola_init", "successful");
-        HolaPay.logPayment("123456789", "purchase_json", "purchase_signature");
-
-    }
+//    public void setHolyAnalysis(){
+//        HolaAnalysis.init(getApplicationContext(), "888888", "32400");
+//        String openID = HolaAnalysis.getOpenId(AppActivity.this);
+//        Log.e("analysis_openid", openID);
+//        HolaAnalysis.setCustomerId("666666");
+//        HolaAnalysis.log("hola_init", "successful");
+//        HolaPay.logPayment("123456789", "purchase_json", "purchase_signature");
+//
+//    }
 }
